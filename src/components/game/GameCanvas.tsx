425e2a9ceda14c970866911e24ee161cba7993fe
BasiliscX <juniornavarro1988@gmail.com>
2024-10-14 22:14:32 -0300

    refactor: Update GameCanvas to include background music and handle game over state

--- src/components/game/GameCanvas.tsx
+++ src/components/game/GameCanvas.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import { useRouter } from "next/navigation"; // Importar el hook useRouter
 import { startGame, resetGame } from "./gameLogic";
 import GameOverMenu from "./GameOverMenu";
 import Score from "./Score";
@@ -9,8 +8,8 @@ import BrutalistBox from "../box/BrutalistBox";
 import WelcomeScreen from "./WelcomeScreen";
 
 export default function GameCanvas() {
-  const router = useRouter(); // Hook para la navegación
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const audioRef = useRef<HTMLAudioElement | null>(null); // Referencia al audio
   const [isGameOver, setIsGameOver] = useState(false);
   const [score, setScore] = useState(0);
   const [showWelcome, setShowWelcome] = useState(true); // Estado para mostrar la pantalla de bienvenida
@@ -21,6 +20,9 @@ export default function GameCanvas() {
       const ctx = canvas.getContext("2d");
       if (ctx) {
         startGame(canvas, ctx, setIsGameOver, setScore); // Iniciar la lógica del juego con setScore
+        if (audioRef.current) {
+          audioRef.current.play(); // Iniciar la música de fondo al iniciar el juego
+        }
       }
     }
   };
@@ -34,7 +36,15 @@ export default function GameCanvas() {
   const handleRetry = () => {
     setIsGameOver(false);
     setScore(0); // Reiniciar el puntaje al reiniciar el juego
-    router.push("/"); // Redirigir a la página principal
+    resetGame(
+      canvasRef.current!,
+      canvasRef.current!.getContext("2d")!,
+      setIsGameOver,
+      setScore
+    );
+    if (audioRef.current) {
+      audioRef.current.play(); // Reanudar la música al reiniciar el juego
+    }
   };
 
   const handleContinue = () => {
@@ -45,15 +55,30 @@ export default function GameCanvas() {
       setIsGameOver,
       setScore
     );
+    if (audioRef.current) {
+      audioRef.current.play(); // Reanudar la música al continuar
+    }
   };
 
   const handleStartGame = () => {
     setShowWelcome(false); // Ocultar la pantalla de bienvenida y empezar el juego
   };
 
+  useEffect(() => {
+    if (isGameOver && audioRef.current) {
+      audioRef.current.pause(); // Pausar la música cuando se acaba el juego
+      audioRef.current.currentTime = 0; // Reiniciar la música al comienzo
+    }
+  }, [isGameOver]);
+
   return (
     <BrutalistBox style={{ position: "relative" }}>
       {showWelcome && <WelcomeScreen onStart={handleStartGame} />}
+      <audio
+        ref={audioRef}
+        src="/sounds/game/Juhani Junkala [Chiptune Adventures] 1. Stage 1.ogg"
+        loop // Repetir en bucle
+      />
       <canvas ref={canvasRef} width={800} height={600} />
       <Score score={score} />
       {isGameOver && (
