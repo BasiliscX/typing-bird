502e479b5964d2e37e92c18197617f4d9e986978
BasiliscX <juniornavarro1988@gmail.com>
2024-10-15 01:33:34 -0300

    refactor: Update GamePage styling in GameCanvas component

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -4,8 +4,8 @@ import GameCanvas from "@/components/game/GameCanvas";
 export default function GamePage() {
   return (
     <div>
-      <Title className="hTitle" title="TypingBird" />
-      <div className="flex justify-center mt-10">
+      <Title className="hTitle bg-amber-50 text-slate-900" title="TypingBird" />
+      <div className="flex justify-center mt-4">
         <GameCanvas />
       </div>
     </div>
