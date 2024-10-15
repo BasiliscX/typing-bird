f659e79779c77729fc6822a0eefd878b57b8b737
BasiliscX <juniornavarro1988@gmail.com>
2024-10-15 10:00:30 -0300

    refactor: Update homepage container background color and game rules section
    - Update homepage container background color to improve visual appeal
    - Remove unnecessary game rules from the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 // src/app/page.tsx
 import Link from "next/link";
 import GameRules from "@/components/home/GameRules";
@@ -8,24 +9,36 @@ export default function HomePage() {
       <div className="grid grid-rows-[auto,1fr,auto] gap-8 h-full">
         {/* Título */}
         <div className="row-span-1 flex items-center justify-center bg-white shadow-md p-6 rounded-md">
-          <h1 className="text-5xl font-bold text-gray-800">
-            Welcome to the Game Page
-          </h1>
+          <h1 className="text-5xl font-bold text-gray-800">Typing Birds!</h1>
         </div>
 
         {/* Enlaces de Navegación */}
         <div className="row-span-1 grid grid-cols-1 md:grid-cols-2 gap-8">
           <Link
             href="/game"
-            className="bg-blue-500 text-white py-8 px-4 text-2xl rounded-lg hover:bg-blue-600 transition-all flex items-center justify-center"
+            className="relative bg-blue-500 text-white py-8 px-4 text-2xl rounded-lg hover:bg-blue-600 transition-all flex items-center justify-center"
           >
-            Go to Game
+            <p className="font-impact text-slate-50 bg-slate-950 p-2">
+              Play Game
+            </p>
+            <img
+              src="/images/home/fb-game.svg"
+              alt="Play Game"
+              className="absolute inset-0 w-full h-full object-cover opacity-20"
+            />
           </Link>
           <Link
-            href="/scores"
-            className="bg-green-500 text-white py-8 px-4 text-2xl rounded-lg hover:bg-green-600 transition-all flex items-center justify-center"
+            href="/about"
+            className="relative bg-green-500 text-white py-8 px-4 text-2xl rounded-lg hover:bg-green-600 transition-all flex items-center justify-center"
           >
-            Go to Scores
+            <p className="font-impact text-slate-50 bg-slate-950 p-2">
+              About this game and credits.
+            </p>
+            <img
+              src="/images/home/fb-about.svg"
+              alt="Play Game"
+              className="absolute inset-0 w-full h-full object-cover opacity-20"
+            />
           </Link>
         </div>
 
--- src/components/home/GameRules.tsx
+++ src/components/home/GameRules.tsx
@@ -4,15 +4,10 @@ export default function GameRules() {
   return (
     <section className="text-gray-600 text-lg text-center">
       <h2 className="text-3xl font-bold text-gray-800 mb-4">Game Rules</h2>
-      <ul className="list-disc list-inside">
+      <ul className="">
         <li className="mb-2">
           Press the correct key to jump over obstacles. Each correct jump
           increases your score.
-        </li>
-        <li className="mb-2">
-          The first few obstacles can be jumped with the &quot;Space&quot; key,
-          but as the game progresses, you&apos;ll need to use specific letter
-          keys.
         </li>
         <li className="mb-2">
           The game ends if the bird collides with an obstacle or touches the
